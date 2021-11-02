ae4f8279f1040b4e807b62f79c6c0bd6fedd1f06
fotoz <fotozxx@gmail.com>
2021-11-02 12:53:24 +0200

    update #3

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,16 @@ $(function () {
 
 //===== User scripts:
 
+//===== Smooth scroll:
+$('a[href^="#"]:not([href="#"])').on('click', function(event) {
+  event.preventDefault();
+
+  $('html,body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 350);
+});
+
+
 //===== Touch event check:
 if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
   document.body.classList.add('touch');
